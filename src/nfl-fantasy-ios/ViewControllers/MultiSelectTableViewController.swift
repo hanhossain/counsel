515005e7607b27d9cc737fe4573d8a3a0d8071ac
Han Hossain <ahanuf12@gmail.com>
2018-10-21 22:12:45 -0700

    unselect after select in multi select view controller

--- src/nfl-fantasy-ios/ViewControllers/MultiSelectTableViewController.swift
+++ src/nfl-fantasy-ios/ViewControllers/MultiSelectTableViewController.swift
@@ -15,7 +15,6 @@ class MultiSelectTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
         return data.count
     }
 
@@ -23,53 +22,18 @@ class MultiSelectTableViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "multiSelectCell", for: indexPath)
 		
 		cell.textLabel?.text = data[indexPath.row]
+//		cell.accessoryType = .checkmark
 
         return cell
     }
+	
+	// MARK: - Table view delegate
+	
+	override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+		guard let cell = tableView.cellForRow(at: indexPath) else { return }
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+//		cell.accessoryType = cell.accessoryType == .none ? .checkmark : .none
+		cell.setSelected(false, animated: false)
+	}
 
 }
