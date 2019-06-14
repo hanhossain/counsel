329446840e57690ef0c6613319fe028584731dec
Han Hossain <ahanuf12@gmail.com>
2019-06-13 23:16:51 -0700

    some cleanup

--- src/Counsel/ViewControllers/PlayerDetailViewController.swift
+++ src/Counsel/ViewControllers/PlayerDetailViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class PlayerDetailViewController: UICollectionViewController {
-	private let _cellId = "Cell"
+	private let _cellId = "playerDetailCell"
 	
 	var fantasyService: FantasyService?
 	var playerId: String?
@@ -18,8 +18,7 @@ class PlayerDetailViewController: UICollectionViewController {
         super.viewDidLoad()
 
         // Register cell classes
-        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: _cellId)
-
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: _cellId)
         // Do any additional setup after loading the view.
 		guard let playerId = playerId,
 			let fantasyService = fantasyService else { return }
@@ -28,46 +27,28 @@ class PlayerDetailViewController: UICollectionViewController {
 		title = player.name
     }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
-    }
-    */
-
     // MARK: UICollectionViewDataSource
 	
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of items
-        return 0
+        return 1
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: _cellId, for: indexPath)
-    
+		
         // Configure the cell
+		cell.backgroundColor = .black
     
         return cell
     }
+}
 
-    // MARK: UICollectionViewDelegate
-
-    /*
-    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
-    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
-    
-    }
-    */
-
+extension PlayerDetailViewController: UICollectionViewDelegateFlowLayout {
+	func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+		let padding: CGFloat = 8
+		let totalPadding: CGFloat = padding * 2
+		let width = UIScreen.main.bounds.width - totalPadding
+		
+		return CGSize(width: width, height: 250)
+	}
 }
